e62c6dbc14c82b04fc52f3b620675ebb10281575
vagrant <vagrant@servidorct.homedamain.com.br>
2020-06-29 21:17:13 -0300

    trocando a valor secret

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fetch = require("node-fetch");
 require('dotenv').config();
-const OWM_API_KEY = process.env.OWM_API_KEY || 'invalid_key';
+const OWM_API_KEY = process.env.default || 'invalid_key';
 const UNITS = process.env.UNITS || 'metric';
 
 /* GET home page. */
@@ -12,7 +12,7 @@ router.get('/', function(req, res) {
 
 router.post('/get_weather', async function (req,res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=027a2abc04d67ba0535db3a883a2e030`;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`;
 
   try {
     let data = await fetch(url);
